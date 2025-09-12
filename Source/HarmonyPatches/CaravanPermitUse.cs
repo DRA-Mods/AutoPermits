217fd849cde23f6c27d35eb90431613c7f5af68a
SokyranTheDragon <exploder1531@gmail.com>
2025-09-12 17:30:19 +0200

    1.6 update, 1.6 fixes
    In 1.6, the permit cooldown messages don't always trigger due to a bug with VTR. Since this mod was relying on it I've included a workaround around that issue.

--- Source/HarmonyPatches/CaravanPermitUse.cs
+++ Source/HarmonyPatches/CaravanPermitUse.cs
@@ -1,7 +1,7 @@
-﻿using AutoPermits.Utilities;
+﻿using System;
+using AutoPermits.Utilities;
 using HarmonyLib;
 using RimWorld;
-using RimWorld.Planet;
 using Verse;
 
 namespace AutoPermits.HarmonyPatches;
@@ -9,17 +9,18 @@ namespace AutoPermits.HarmonyPatches;
 [HarmonyPatch(typeof(Pawn_RoyaltyTracker), nameof(Pawn_RoyaltyTracker.RoyaltyTrackerTickInterval))]
 public static class CaravanPermitUse
 {
-    public static void Postfix(Pawn_RoyaltyTracker __instance)
+    public static void Postfix(Pawn_RoyaltyTracker __instance, int delta)
     {
         // If a pawn is spawned on a map, the check will be done each tick anyway.
         // Also check if the pawn will ever use the permits on caravan, and (like vanilla) exclude animals from this.
-        if (!__instance.pawn.IsCaravanMember() || !__instance.UsePermitsOnCaravans() || __instance.pawn.RaceProps.Animal)
-            return;
+        // TODO: Seems vanilla isn't always triggering the permit messages due to VTR, revert once it's fixed
+        // if (!__instance.pawn.IsCaravanMember() || !__instance.UsePermitsOnCaravans() || __instance.pawn.RaceProps.Animal)
+        //     return;
 
         // Same loop that's used in Vanilla, but only when a pawn is spawned and non-animal
         foreach (var permit in __instance.AllFactionPermits)
         {
-            if (permit.LastUsedTick > 0 && Find.TickManager.TicksGame == permit.LastUsedTick + permit.Permit.CooldownTicks)
+            if (permit.LastUsedTick > 0 && Math.Abs(Find.TickManager.TicksGame - permit.LastUsedTick - permit.Permit.CooldownTicks) < delta)
                 permit.TryTriggerPermit(__instance.pawn);
         }
     }
